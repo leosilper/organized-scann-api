730aa37c3eff0f9d762252af9c7c1beea9964364
leosilper <leo10silva440@gmail.com>
2025-04-28 01:22:49 -0300

    implementando configuração de CORS e Security básica e criando entidades Moto e Portal.

---
+++ organized-scann-api/src/main/java/br/com/fiap/organized_scann_api/model/Portal.java
@@ -0,0 +1,29 @@
+package br.com.fiap.organized_scann_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Portal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PortalTipo tipo;
+
+    @NotBlank
+    private String nome;
+}
---
+++ organized-scann-api/src/main/java/br/com/fiap/organized_scann_api/model/PortalTipo.java
@@ -0,0 +1,8 @@
+package br.com.fiap.organized_scann_api.model;
+
+public enum PortalTipo {
+    MANUTENCAO_RAPIDA,
+    MANUTENCAO_DEMORADA,
+    BOLETIM_OCORRENCIA,
+    MOTO_RECUPERADA
+}
