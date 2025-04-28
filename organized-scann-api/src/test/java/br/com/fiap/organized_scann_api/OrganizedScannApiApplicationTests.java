3b495d5bf24bc762c50288df3bc6d533469ae958
leosilper <leo10silva440@gmail.com>
2025-04-28 01:46:19 -0300

    Specification para filtros de Moto representando buscas dinâmicas por parâmetros; ValidationHandler para tratamento de erros; Implementação do DatabaseSeeders para dados iniciais e OrganizedScannApiApplicationTests para testes iniciais do projeto.

---
+++ organized-scann-api/src/test/java/br/com/fiap/organized_scann_api/OrganizedScannApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.organized_scann_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizedScannApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
