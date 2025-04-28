590bcfa523ee121ad108de26463a944179a945cc
leosilper <leo10silva440@gmail.com>
2025-04-28 10:18:04 -0300

    Criação do README com a descrição do projeto,  e implementação do GlobalExceptionHandler paraa melhoria do tratamento centralizado de erros.

---
+++ organized-scann-api/src/main/java/br/com/fiap/organized_scann_api/handler/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package br.com.fiap.organized_scann_api.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Captura erros de validação (@Valid)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> 
+            errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return errors;
+    }
+
+    // Captura qualquer outra exceção não tratada
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllUncaughtException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro inesperado: " + ex.getMessage());
+    }
+
+}
