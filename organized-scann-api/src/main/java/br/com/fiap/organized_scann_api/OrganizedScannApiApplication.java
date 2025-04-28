f002a3727c08cf09a54de38ff2819a6fbbb47527
leosilper <leo10silva440@gmail.com>
2025-04-27 22:13:52 -0300

    Criando estrutura inicial do projeto Organized Scann API

---
+++ organized-scann-api/src/main/java/br/com/fiap/organized_scann_api/OrganizedScannApiApplication.java
@@ -0,0 +1,16 @@
+package br.com.fiap.organized_scann_api;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "Organized Scann API", version = "1.0", description = "API para gestão de motos por portais de manutenção"))
+public class OrganizedScannApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizedScannApiApplication.class, args);
+    }
+}
